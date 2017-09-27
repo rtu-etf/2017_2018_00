507a456cabeaa7b1bdaa6e1a067761595559a111
ansiiis1@gmail.com <Tatjana.Solovjova@rtu.lv>
2017-09-27 14:09:11 +0300

    20170927_14_10

---
+++ shell_examples.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#3. piemērs
+NAME="Vārds Uzvārds"
+echo $NAME
+unset NAME
+echo $NAME
+
+#2. piemērs
+#NAME="Vārds Uzvārds"
+#readonly NAME
+#echo $NAME
+#NAME="Uzvārds Vārds"
+#echo $NAME
+
+#1. piemērs
+#NAME="Vārds Uzvārds"
+#echo $NAME
+
+#0. piemērs
+#history > history_20170927a.txt
