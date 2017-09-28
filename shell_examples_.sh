0cd673a6a0097baefb651fddbfee4fafb9626d78
ansiiis1@gmail.com <Tatjana.Solovjova@rtu.lv>
2017-09-28 09:51:52 +0300

    20170928_09_50

---
+++ shell_examples_.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#4. piemērs
+echo "Atbilde uz jautājumu - kas šobrīd tiek pildīts? "$0
+#echo $n
+echo "Skriptam nodoto argumentu skaits: "$#
+
+#3. piemērs
+#NAME="Vārds Uzvārds"
+#echo $NAME
+#unset NAME
+#echo $NAME
+
+#2. piemērs
+#NAME="Vārds Uzvārds"
+#readonly NAME
+#echo $NAME
+#NAME="Uzvārds Vārds"
+#echo $NAME
+
+#1. piemērs
+#NAME="Vārds Uzvārds"
+#echo $NAME
