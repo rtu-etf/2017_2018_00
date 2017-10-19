5a44e9cef88829dc5d7922d528fdab5b34694381
roberts.mukans@gmail.com <Tatjana.Solovjova@rtu.lv>
2017-10-19 12:05:38 +0300

    20171019_12_05

---
+++ a_VS_b_VS_c_C03.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+echo "Input a:"
+read a
+echo "Input b:"
+read b
+
+if (( $a == $b ))
+then
+echo "a ($a) ir vienāds ar b ($b)"
+elif (( $a > $b ))
+then
+echo "a ($a) ir lielāks par b ($b)"
+else
+echo "a ($a) ir mazāks par b ($b)"
+fi
+
+
+
+
+
+
+
+
+: <<'END'
+if [ $a -eq $b ]
+then
+echo "a ($a) ir vienāds ar b ($b)"
+fi
+if [ $a -gt $b ]
+then
+echo "a ($a) ir lielāks par b ($b)"
+fi
+if [ $a -lt $b ]
+then
+echo "a ($a) ir mazāks par b ($b)"
+fi
+END
+
+
+
+
+: <<'END'
+if [ $a -eq $b ]
+then
+echo "a ($a) ir vienāds ar b ($b)"
+fi
+END
