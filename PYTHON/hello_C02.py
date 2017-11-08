90dff7f08ff8dae8d268f810fd742fcd53ef0c68
roberts.mukans@gmail.com <Tatjana.Solovjova@rtu.lv>
2017-11-08 11:51:11 +0200

    20171108_11_50

---
+++ PYTHON/hello_C02.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# 3. piemērs
+print "Here \bthe \bspaces \bare \bbackspaced."
+print "\\b - nozīme ..."
+
+'''
+# 1. piemērs
+print "Hello World!"
+'''
