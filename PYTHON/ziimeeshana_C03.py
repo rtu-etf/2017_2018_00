3883a766fa739b6bb3de7c950948f8ef75ff7c6c
roberts.mukans@gmail.com <Tatjana.Solovjova@rtu.lv>
2017-11-30 12:13:21 +0200

    20171130_12_15

---
+++ PYTHON/ziimeeshana_C03.py
@@ -0,0 +1,94 @@
+#https://matplotlib.org/users/pyplot_tutorial.html
+
+import matplotlib.pyplot as plt
+#plt.plot([1,2,3,4], [1,4,9,16], 'or')
+plt.plot([1,2,3,4], [1,4,9,16], 'g^',markersize=30)
+plt.plot([1,2,3,4], [1,4,9,16])
+plt.plot([1,2,3,4], [1,4,9,16], 'k:',linewidth=2)
+plt.axis([0, 6, 0, 20])
+plt.show()
+
+
+'''
+import numpy as np
+import matplotlib.pyplot as plt
+
+from matplotlib.ticker import NullFormatter  # useful for `logit` scale
+
+# Fixing random state for reproducibility
+np.random.seed(19680801)
+
+# make up some data in the interval ]0, 1[
+y = np.random.normal(loc=0.5, scale=0.4, size=1000)
+y = y[(y > 0) & (y < 1)]
+y.sort()
+x = np.arange(len(y))
+
+# plot with various axes scales
+plt.figure(1)
+
+# linear
+plt.subplot(221)
+plt.plot(x, y)
+plt.yscale('linear')
+plt.title('linear')
+plt.grid(True)
+
+
+# log
+plt.subplot(222)
+plt.plot(x, y)
+plt.yscale('log')
+plt.title('log')
+plt.grid(True)
+
+
+# symmetric log
+plt.subplot(223)
+plt.plot(x, y - y.mean())
+plt.yscale('symlog', linthreshy=0.01)
+plt.title('symlog')
+plt.grid(True)
+
+# logit
+#plt.subplot(224)
+#plt.plot(x, y)
+#plt.yscale('logit')
+#plt.title('logit')
+#plt.grid(True)
+# Format the minor tick labels of the y-axis into empty strings with
+# `NullFormatter`, to avoid cumbering the axis with too many labels.
+plt.gca().yaxis.set_minor_formatter(NullFormatter())
+# Adjust the subplot layout, because the logit one may take more space
+# than usual, due to y-tick labels like "1 - 10^{-3}"
+plt.subplots_adjust(top=0.92, bottom=0.08, left=0.10, right=0.95, hspace=0.25,
+                    wspace=0.35)
+
+plt.show()
+'''
+
+'''
+import matplotlib.pyplot as plt
+plt.figure(1)                # the first figure
+plt.subplot(211)             # the first subplot in the first figure
+plt.plot([1, 2, 3])
+plt.subplot(212)             # the second subplot in the first figure
+plt.plot([4, 5, 6])
+
+
+plt.figure(2)                # a second figure
+plt.plot([4, 5, 6])          # creates a subplot(111) by default
+
+plt.figure(1)                # figure 1 current; subplot(212) still current
+plt.subplot(211)             # make subplot(211) in figure1 current
+plt.title('Easy as 1, 2, 3') # subplot 211 title
+plt.show()
+'''
+
+'''
+import matplotlib.pyplot as plt
+plt.plot([1,2,3,4,5,6])
+plt.ylabel('some numbers')
+plt.xlabel('some other numbers')
+plt.show()
+'''
