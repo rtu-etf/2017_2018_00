663baa617b0c333c2a20fdf4e49f5272c77e7ec3
roberts.mukans@gmail.com <Tatjana.Solovjova@rtu.lv>
2017-12-13 16:05:04 +0200

    20171213_16_05

--- PYTHON/quizNr1.py
+++ PYTHON/quizNr1.py
@@ -1,24 +1,36 @@
 import random
 import numpy as np
 
-def ranges(b_r,b,x):
-    for i in range(len(x)):
-        for j in range(len(b_r)-1):
-            if x[i] > b_r[j] and x[i] < b_r[j+1]:
-                b[j] = b[j] + 1
-    return b
+def ranges(bars_ranges_local,bars_local,x_local):
+    for i in range(len(x_local)):
+        for j in range(len(bars_ranges_local)-1):
+            if x_local[i] > bars_ranges_local[j] \
+               and x_local[i] < bars_ranges_local[j+1]:
+                bars_local[j] = bars_local[j] + 1
+                #print x_local[i]
+                #print bars_local
+        if x_local[i] > bars_ranges_local[j+1]:
+            bars_local[j+1] = bars_local[j+1] + 1
+            #print x_local[i]
+            #print bars_local
+    return bars_local
 
-N = 100000
+N = 5
 x = []
 a = 0
 b = np.pi
 delta = 0.5
-for i in range(N):
-    #x.append(random.uniform(a,b))
-    x.append(random.triangular(a,b))
+random.seed(0)
+for i in range(N):   
+    x.append(random.uniform(a,b))
+    #x.append(random.triangular(a,b))
+    #print x
+print x
 
 bars_ranges = np.arange(a,b,delta)
+print bars_ranges
 bars = np.zeros(len(bars_ranges))
 print bars
 bars = ranges(bars_ranges,bars,x)
 print bars
+print bars.sum()
