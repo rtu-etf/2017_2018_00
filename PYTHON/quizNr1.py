e607835239584a8207bb572fe40b2795fc7cef8e
roberts.mukans@gmail.com <Tatjana.Solovjova@rtu.lv>
2017-12-13 12:10:18 +0200

    20171213_12_10

--- PYTHON/quizNr1.py
+++ PYTHON/quizNr1.py
@@ -8,16 +8,17 @@ def ranges(b_r,b,x):
                 b[j] = b[j] + 1
     return b
 
-N = 100
+N = 100000
 x = []
 a = 0
 b = np.pi
 delta = 0.5
 for i in range(N):
-    x.append(random.uniform(a,b))
+    #x.append(random.uniform(a,b))
+    x.append(random.triangular(a,b))
 
 bars_ranges = np.arange(a,b,delta)
 bars = np.zeros(len(bars_ranges))
-
+print bars
 bars = ranges(bars_ranges,bars,x)
 print bars
